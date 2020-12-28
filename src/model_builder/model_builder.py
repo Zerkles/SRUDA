c560ed61951c96abff3b786d9d18d6920564a8a0
Piotr Kwiatkowski <kwiat1998PP@gmail.com>
2020-12-28 15:19:29 +0100

    added model builder

---
+++ src/model_builder/model_builder.py
@@ -0,0 +1,87 @@
+#!/home/kwitnoncy/anaconda3/envs/inz/bin/python
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import tree
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+from sklearn.metrics import accuracy_score
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+from typing import List
+
+
+def prepare_data(filename: str, 
+                 separator: str = '',
+                 useless_columns: List[str] = [],
+                 labels_header: str = '', 
+                 labels: List[int] = []
+                 ) -> (int, np.ndarray, List[int]):
+    data = None
+
+    try:
+        if separator:
+            data = pd.read_csv(filename, sep=separator)
+        else:
+            data = pd.read_csv(filename, sep='\t')
+    except FileNotFoundError:
+        print('No file named: ' + filename)
+        return (1, None, None)
+
+
+    if data.empty:
+        return (2, None, None)
+
+    current_header = list(data.columns)
+
+    for h in useless_columns:
+        try:
+            current_header.remove(h)
+        except ValueError:
+            print('No column named: ', h)
+            return (3, None, None)
+
+    if labels:
+        # load labels to y
+        pass
+    elif labels_header:
+        current_header.remove(labels_header)
+        y = data[labels_header]
+    else:
+        return (4, None, None)
+
+    X = data.loc[:, current_header]
+    X = X.to_numpy()
+    y = y.to_numpy()
+    
+    return (0, X, y)
+    
+
+def split(X: np.ndarray, y: List[int]) -> (np.ndarray, np.ndarray, List[int], List[int]):
+    return train_test_split(X, y, test_size=0.2, random_state=7)
+
+
+def teach_model(model_name: str, X: (np.ndarray, np.ndarray), y: (List[int], List[int])) -> float:
+    model = None
+    returning = False
+
+    if model_name == 'XGB':
+        returning = False
+        model = XGBClassifier()
+    elif model_name == 'log_reg':
+        returning = True
+        model = LogisticRegression(random_state=0, solver='sag', max_iter=10000000)
+    elif model_name == 'tree':
+        returning = True
+        model = tree.DecisionTreeClassifier()
+    elif model_name == 'cat':
+        returning = True
+        model = None
+
+
+
+
+print(prepare_data(filename='data_100k.csv', separator='\t', useless_columns=['SalesAmountInEuro', 'time_delay_for_conversion'], labels_header="Sales"))
+
---
+++ src/model_builder/model_builder_tree.py
@@ -0,0 +1,44 @@
+#!/home/kwitnoncy/anaconda3/envs/inz/bin/python
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from collections import Counter
+import numpy as np
+import pandas as pd
+
+
+# open file with balanced and unbalanced data
+data = pd.read_csv('data_100k.csv', sep='\t')
+# data = pd.read_csv('whole_formated.csv', sep='\t')
+print(data.columns)
+#X = data.loc[:, data.columns != ['Sales', 'SalesAmountInEuro',
+#                                 'time_delay_for_conversion']].to_numpy()
+X = data.loc[:, ['click_timestamp', 'nb_clicks_1week', 'product_price',
+     'product_age_group', 'device_type', 'audience_id', 'product_gender',
+     'product_brand', 'prod_cat_1', 'prod_cat_3', 'prod_cat_4', 'prod_cat_5',
+     'prod_cat_6', 'prod_cat_7', 'product_country', 'product_id',
+     'product_title', 'partner_id', 'user_id']]
+y = data['Sales']
+
+X = X.to_numpy()
+y = y.to_numpy()
+
+# do some stuff with data. Maybe check what parameters are relevant
+X[np.isnan(X)] = 0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=40)
+print("pre balanced:", sorted(Counter(y_test).items()))
+print("pre balanced:", sorted(Counter(y_train).items()))
+
+tree_clf = tree.DecisionTreeClassifier()
+tree_clf = tree_clf.fit(X_train, y_train)
+
+y_predicted = tree_clf.predict(X_test)
+
+counter = 0
+for i in range(len(y_predicted)):
+    counter += 1 if y_predicted[i] != y_test[i] else 0
+
+print(tree_clf.score(X_test, y_test))
+print(counter)
+
+
